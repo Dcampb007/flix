04ee0cfdb41da7d0275408ab7d1449bbc14be8d1
Andre Campbell <campbellandre97@gmail.com>
2018-09-02 14:17:02 -0400

    Finished first two stretch goals, user can search now

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 import AlamofireImage
 import PKHUD
 
-class NowPlayingViewController: UIViewController, UITableViewDataSource {
+class NowPlayingViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     var movies: [[String: Any]] = []
+    var filteredMovies: [[String: Any]] = []
     var refreshControl: UIRefreshControl!
-    
+    let alertController = UIAlertController(title: "Cannot Get Movies", message: "The internet connection appeas to be offline.", preferredStyle: .alert)
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
@@ -24,7 +26,15 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         refreshControl.addTarget(self, action: #selector (NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
+        searchBar.delegate = self
         fetchMovies()
+        // create a cancel action
+        let retryAction = UIAlertAction(title: "retry", style: .default) { (action) in
+            self.didPullToRefresh(self.refreshControl)
+        }
+        // add the cancel action to the alertController
+        alertController.addAction(retryAction)
+
         
         
         
@@ -42,12 +52,16 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         let task = session.dataTask(with: request) { (data, response, error) in
         // this will run when the network request returns
         if let error = error {
-        print(error.localizedDescription)
+            self.present(self.alertController, animated: true) {
+                // optional code for what happens after the alert controller has finished presenting
+                print(error.localizedDescription)
+            }
         }
         else if let data = data {
         let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
         let movies = dataDictionary["results"] as! [[String: Any]]
         self.movies = movies
+        self.filteredMovies = self.movies
         self.tableView.reloadData()
         self.refreshControl.endRefreshing()
     
@@ -56,11 +70,11 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         task.resume()
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        return filteredMovies.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for:indexPath) as! MovieCell
-        let movie = movies[indexPath.row]
+        let movie = filteredMovies[indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         cell.titleLabel.text = title
@@ -69,5 +83,20 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         let posterUrl = URL(string: "https://image.tmdb.org/t/p/w500" + posterPathString)!
         cell.posterImageView.af_setImage(withURL: posterUrl)
         return cell
+    }
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        filteredMovies = searchText.isEmpty ? movies : movies.filter { (item: [String:Any]) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            let title = item["title"] as! String
+            return title.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        tableView.reloadData()
     }
    }
